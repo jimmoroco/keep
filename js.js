943dc4ee624a141b6a5f320b569f4626899b9a09
Jim Moroco <jim.moroco@gmail.com>
2024-03-11 21:26:53 -0500

    feat: modify styles and html structured

--- js.js
+++ js.js
@@ -1,21 +1,21 @@
 const INPUT_CHECKBOX = "<input type='checkbox' class='checkbox-item appearance' onchange='checkedItem(this)'/>";
+
 let hasNextElement;
 let square;
+
 window.addEventListener("load", function () {
     hasNextElement = false;
     square = document.getElementsByClassName("square")[0];
     let title = document.getElementsByClassName("title")[0];
-    let element_0 = document.getElementsByClassName("element-0")[0];
+    let element = document.getElementsByClassName("element")[0];
     title.addEventListener("keypress", function (event) {
         let keycode = (event.keyCode ? event.keyCode : event.which);
         if (keycode == '13') {
-            element_0.focus();
+            element.focus();
         }
     });
-    element_0.addEventListener("keypress", function (event) {
-        console.log(event);
-        console.log(event.target);
-        nextElement(event, this);
+    element.addEventListener("keypress", function (event) {
+        nextElement(event);
     });
 });
 
@@ -23,11 +23,12 @@ function checkedItem(checkBox) {
     checkBox.classList.toggle("appearance");
 }
 
-function nextElement(event, previousElement) {
+function nextElement(event) {
     if (!hasNextElement) {
         square.insertAdjacentHTML("beforeend", getPendingItem());
+        
+        let previousElement = event.target;
         previousElement.previousSibling.previousSibling.innerHTML = INPUT_CHECKBOX;
-        console.log("jim");
         hasNextElement = true;
     }
     let keycode = (event.keyCode ? event.keyCode : event.which);
@@ -40,7 +41,7 @@ function getPendingItem() {
     let html = "";
     html += "<div class='item'>";
     html += "<span class='plus'>&#128930;</span>";
-    html += "<div class='element-0 element' contenteditable='true' placeholder='Elemento de lista'></div>";
+    html += "<div class='element' contenteditable='true' placeholder='Elemento de lista'></div>";
     html += "</div>";
     return html;
 }
