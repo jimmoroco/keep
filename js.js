3e78e1ee1a55d39dc988b109cef8c76e9666ed6b
Jim Moroco <jim.moroco@gmail.com>
2024-03-15 22:26:20 -0500

    feat: create square edit

--- js.js
+++ js.js
@@ -1,22 +1,68 @@
 const INPUT_CHECKBOX = "<input type='checkbox' class='checkbox-item appearance' onchange='checkedItem(this)'/>";
+const DIV_SQUARE_EDIT = "<div id = 'div-title' class='title edit' contenteditable='true' placeholder='Título'></div><div id = 'div-item' class='item edit'><span class='plus edit'>&#128930;</span><input type='text' class='element edit' placeholder='Elemento de lista' data-has-next-element = '0' id='element-0'></div>";
 
-let hasNextElement;
 let square;
+let elementNumber = 0;
+let elementId;
+let qSquares;
+let title;
+
+document.addEventListener("click", function (e) {
+    const classList = e.target.classList;
+    let clickInSquareEdit = false;
+    for (let i = 0; i < classList.length; i++) {
+        if (classList[i] == "edit") {
+            clickInSquareEdit = true;
+            break;
+        }
+    }
+    if (!clickInSquareEdit) {
+        console.log("!clickInSquareEdit");
+        let divSquareEdit = document.getElementById("div-square-edit");
+        console.log("title",title);
+        if (title.innerText != "") {
+            console.log("ddd");
+            localStorage.setItem(`square-${qSquares++}`, divSquareEdit.innerHTML);
+        }
+        divSquareEdit.classList.add("hidden");
+    }
+});
 
 window.addEventListener("load", function () {
-    hasNextElement = false;
+    qSquares = localStorage.getItem("qSquares");
+    if (qSquares == null) localStorage.setItem("qSquares", 0);
+
     square = document.getElementsByClassName("square")[0];
-    let title = document.getElementsByClassName("title")[0];
+
+    title = document.getElementsByClassName("title")[0];
     let element = document.getElementsByClassName("element")[0];
+    let iconPlus = document.getElementById("icon-plus")
+    let divSquareEdit = document.getElementById("div-square-edit");
+
     title.addEventListener("keypress", function (event) {
         let keycode = (event.keyCode ? event.keyCode : event.which);
         if (keycode == '13') {
             element.focus();
         }
     });
+
     element.addEventListener("keypress", function (event) {
         nextElement(event);
     });
+
+    iconPlus.addEventListener("click", function (e) {        
+        divSquareEdit.classList.remove("hidden");
+        //divSquareEdit.innerHTML = DIV_SQUARE_EDIT;
+        title.focus();
+    });
+
+    if (qSquares > 0) {
+        let squareItem;
+        for (let index = 1; index <= qSquares; index++) {
+            squareItem = array[index];
+
+        }
+    }
 });
 
 function checkedItem(checkBox) {
@@ -24,24 +70,38 @@ function checkedItem(checkBox) {
 }
 
 function nextElement(event) {
-    if (!hasNextElement) {
-        square.insertAdjacentHTML("beforeend", getPendingItem());
-        
-        let previousElement = event.target;
-        previousElement.previousSibling.previousSibling.innerHTML = INPUT_CHECKBOX;
-        hasNextElement = true;
+    let actualElement = event.target;
+    let hasNextElement = actualElement.getAttribute('data-has-next-element');
+    if (hasNextElement == "0") {
+        let parentActualElement = actualElement.parentNode.parentNode;
+        parentActualElement.insertAdjacentHTML("beforeend", getPendingItem());
+        if (event.target.id == "element-0") {
+            actualElement.previousSibling.previousSibling.innerHTML = INPUT_CHECKBOX;
+        }
+        else {
+            actualElement.previousSibling.innerHTML = INPUT_CHECKBOX;
+        }
+        elementId = document.getElementById(elementId);
+        elementId.addEventListener("keypress", function (event) {
+            nextElement(event);
+        });
+        actualElement.setAttribute('data-has-next-element', "1");
     }
     let keycode = (event.keyCode ? event.keyCode : event.which);
     if (keycode == '13') {
-        console.log("13");
+        elementId.focus();
     }
 }
 
 function getPendingItem() {
+    elementNumber++;
+    elementId = "element-" + elementNumber;
     let html = "";
-    html += "<div class='item'>";
-    html += "<span class='plus'>&#128930;</span>";
-    html += "<div class='element' contenteditable='true' placeholder='Elemento de lista'></div>";
+    html += "<div class='item edit'>";
+    html += "<span class='plus edit'>&#128930;</span>";
+    html += "<input type='text' class='element edit' placeholder='Elemento de lista' data-has-next-element = '0' id='";
+    html += elementId;
+    html += "'>";
     html += "</div>";
     return html;
 }
